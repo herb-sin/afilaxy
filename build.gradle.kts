93987a2ec3d684ce1f0c5f3b2e772276ad14ac33
herb-sin <herbertjungsin@gmail.com>
2025-08-24 18:43:57 -0300

    Firebase implementado

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,11 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin) apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
 }
 
 buildscript {
     dependencies {
-        classpath("com.google.gms:google-services:4.4.1")
+        classpath("com.google.gms:google-services:4.4.3")
     }
 }
